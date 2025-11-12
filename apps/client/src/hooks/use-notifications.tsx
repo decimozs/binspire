5e948854cfe588c1c9f1b759309367f6a79cd565
decimozs <marlonadiguemartin548@gmail.com>
2025-11-12 23:10:09 +0800

    update: added invalidation query for assignment when collection and refactor use-notification hook

--- apps/client/src/hooks/use-notifications.tsx
+++ apps/client/src/hooks/use-notifications.tsx
@@ -2,8 +2,11 @@ import { messaging } from "@/features/firebase";
 import { onMessage } from "firebase/messaging";
 import { useEffect } from "react";
 import { toast } from "@binspire/ui/toast";
+import { useQueryClient } from "@binspire/query";
 
 export function useNotifications() {
+  const queryClient = useQueryClient();
+
   useEffect(() => {
     const unsubscribe = onMessage(messaging, (payload) => {
       const notification = payload.notification;
@@ -19,9 +22,18 @@ export function useNotifications() {
             </div>
           </div>
         ));
+
+        queryClient.invalidateQueries({
+          queryKey: ["user-collection-assignments"],
+          refetchType: "all",
+        });
+        queryClient.invalidateQueries({
+          queryKey: ["issues"],
+          refetchType: "all",
+        });
       }
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [queryClient]);
 }
