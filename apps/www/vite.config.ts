9d5e42b4d9cbd5ccccbd847a197f0c77ed8e5105
decimozs <marlonadiguemartin548@gmail.com>
2025-12-07 18:31:12 +0800

    chore: update deps

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from "vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
+  // vercel
+  base: "/",
   envDir: path.resolve(__dirname, "../../"),
   plugins: [
     tanstackRouter({
