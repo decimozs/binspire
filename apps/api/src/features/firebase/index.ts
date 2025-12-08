75c04dbf17fa8e808dcd8d27dd6bd81e65efd176
decimozs <marlonadiguemartin548@gmail.com>
2025-12-08 09:36:26 +0800

    refactor: change path for service-account.json

--- apps/api/src/features/firebase/index.ts
+++ apps/api/src/features/firebase/index.ts
@@ -1,9 +1,18 @@
 import { initializeApp, cert, type ServiceAccount } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
-import serviceAccount from "../../../service-account.json" assert { type: "json" };
+
+const serviceAccountJson = Bun.env.FIREBASE_SERVICE_ACCOUNT_JSON;
+
+if (!serviceAccountJson) {
+  throw new Error(
+    "FATAL: FIREBASE_SERVICE_ACCOUNT_JSON environment variable not found.",
+  );
+}
+
+const serviceAccountConfig = JSON.parse(serviceAccountJson) as ServiceAccount;
 
 initializeApp({
-  credential: cert(serviceAccount as ServiceAccount),
+  credential: cert(serviceAccountConfig),
 });
 
 export const messaging = getMessaging();
