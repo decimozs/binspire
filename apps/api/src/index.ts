202a65856794ff6c92b2da8205150a7048aea44b
decimozs <marlonadiguemartin548@gmail.com>
2025-12-08 08:20:42 +0800

    update: change bootstrap for vercel functions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,21 +3,8 @@ import { PATH_CONFIG } from "./lib/constants";
 import "@binspire/config/load-env";
 import app from "./features/app";
 
-function main() {
-  try {
-    Bun.serve({
-      idleTimeout: 255,
-      port: Bun.env.API_PORT,
-      fetch: app.fetch,
-    });
-    logger.info(
-      `Server is running on http://localhost:${Bun.env.API_PORT}${PATH_CONFIG.VERSION}${PATH_CONFIG.BASE_PATH}`,
-    );
-  } catch (error) {
-    const err = error as Error;
-    logger.error(`Error starting server: ${err.message}`);
-    process.exit(1);
-  }
-}
+logger.info(
+  `Server is running on http://localhost:${Bun.env.API_PORT}${PATH_CONFIG.VERSION}${PATH_CONFIG.BASE_PATH}`,
+);
 
-main();
+export default app;
