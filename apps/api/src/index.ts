a934359b489dfba5a610add76849fc16d12f1424
decimozs <marlonadiguemartin548@gmail.com>
2025-12-08 08:36:38 +0800

    refactor: update api bootstrap method using Bun.serve

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,8 +3,21 @@ import { PATH_CONFIG } from "./lib/constants";
 import "@binspire/config/load-env";
 import app from "./features/app";
 
-logger.info(
-  `Server is running on http://localhost:${Bun.env.API_PORT}${PATH_CONFIG.VERSION}${PATH_CONFIG.BASE_PATH}`,
-);
+function main() {
+  try {
+    Bun.serve({
+      idleTimeout: 255,
+      port: Bun.env.API_PORT,
+      fetch: app.fetch,
+    });
+    logger.info(
+      `Server is running on http://localhost:${Bun.env.API_PORT}${PATH_CONFIG.VERSION}${PATH_CONFIG.BASE_PATH}`,
+    );
+  } catch (error) {
+    const err = error as Error;
+    logger.error(`Error starting server: ${err.message}`);
+    process.exit(1);
+  }
+}
 
-export default app;
+main();
